71d330db40e1a96b0ea6d9467bc30130dce652eb
Joe Maples <frap129@gmail.com>
2014-11-26 15:55:10 -0500

    Add Graphite option for full arm use

--- core/graphite.mk
+++ core/graphite.mk
@@ -31,4 +31,29 @@ ifdef LOCAL_CPPFLAGS
 else
 	LOCAL_CPPFLAGS := $(call cpp-option,$(GRAPHITE_FLAGS))
 endif
+ifdef LOCAL_CPPFLAGS
+	LOCAL_CPPFLAGS += $(call cpp-option,$(GRAPHITE_FLAGS))
+else
+	LOCAL_CPPFLAGS := $(call cpp-option,$(GRAPHITE_FLAGS))
+endif
+ifdef $(combo_2nd_arch_prefix)TARGET_arm_CFLAGS
+	$(combo_2nd_arch_prefix)TARGET_arm_CFLAGS += $(call cc-option,$(GRAPHITE_FLAGS))
+else
+endif
+ifdef $(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS
+	$(combo_2nd_arch_prefix)TARGET_thumb_CFLAGS += $(call cc-option,$(GRAPHITE_FLAGS))
+else
+endif
+ifdef $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS
+	$(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += $(call cc-option,$(GRAPHITE_FLAGS))
+else
+endif
+ifdef $(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS
+	$(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS += $(call cpp-option,$(GRAPHITE_FLAGS))
+else
+endif
+ifdef $(combo_2nd_arch_prefix)TARGET_RELEASE_CFLAGS
+	$(combo_2nd_arch_prefix)TARGET_RELEASE_CFLAGS += $(call cc-option,$(GRAPHITE_FLAGS))
+else
+endif
 #####
